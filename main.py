d26245dc8e85e68690907191f34d2500ee340cda
dmitriikuleshov <dmkuleshov10@gmail.com>
2022-12-12 21:47:22 +0300

    Git и случайные окружности

--- main.py
+++ main.py
@@ -2,14 +2,38 @@ import sys
 import random
 
 from PyQt5 import uic
+from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
 
-class Example(QMainWindow):
+class Ui_Form(object):
+    def setupUi(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(800, 600)
+        Form.setMinimumSize(QSize(800, 600))
+        Form.setMaximumSize(QSize(800, 600))
+        self.pushButton = QPushButton(Form)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(600, 490, 171, 81))
+
+        self.retranslateUi(Form)
+
+        QMetaObject.connectSlotsByName(Form)
+
+    # setupUi
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.pushButton.setText(QCoreApplication.translate("Form", u"Show Circle", None))
+    # retranslateUi
+
+
+class Example(QMainWindow, Ui_Form):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.clicked = False
         self.setWindowTitle('Git и желтые окружности')
         self.pushButton.clicked.connect(self.make_circle)
@@ -19,7 +43,7 @@ class Example(QMainWindow):
 
     def make_circle(self):
         self.circle_size = random.randint(50, 150)
-        # self.color = QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        self.color = QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         self.clicked = True
         self.update()
 
